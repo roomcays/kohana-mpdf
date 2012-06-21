da1d75d2fee7dd90f5aacb48b1d247624ff5ad5e
Roomcays <roomcays@wp.pl>
2012-06-21 12:19:07 +0200

    Extending functionalities: - added write_to_disk() wrapper function for output($filename, 'F') - __toString() magic function now returns string representation of generated PDF using public function as_string(), which is yet another wrapper for output(FALSE, 'S') - some cleaning code and comments

--- classes/view/mpdf/core.php
+++ classes/view/mpdf/core.php
@@ -5,29 +5,12 @@
  * @packge     Kohana-mPDF
  * @author     Woody Gilk <woody.gilk@kohanaphp.com>
  * @author     Sergei Gladkovskiy <smgladkovskiy@gmail.com>
+ * @author     Maciej Kwiatkowski <kodzitsu@gmail.com>
  * @copyright  (c) 2009 Woody Gilk
  * @license    MIT
  */
 abstract class View_mPDF_Core extends View {
 
-	/*
-	protected static $instance = NULL;
-
-	public static function instance()
-	{
-		if (View_mPDF::$instance === NULL)
-		{
-			View_mPDF::$instance = new View_mPDF();
-		}
-
-		return View_mPDF::$instance;
-	}
-
-	public static function get_instance()
-	{
-		return View_mPDF::$instance;
-	}
-	*/
 	protected $mpdf = NULL;
 	protected $view_file = NULL;
 
@@ -42,42 +25,61 @@ abstract class View_mPDF_Core extends View {
 		return new View_MPDF($view_file, $data);
 	}
 
-	public function render($view_file = NULL)
-	{
-		if (empty($view_file)) $view_file = $this->view_file;
-		// Render the HTML normally
-		$html = parent::render($view_file);
-		$this->mpdf->WriteHTML($html);
-		// Render the HTML to a PDF
-		return $this->mpdf->output();
-	}
-
-	public function download($generated_filename, $view_file = NULL)
-	{
-		if (empty($view_file)) $view_file = $this->view_file;
-		// Render the HTML normally
-		$html = parent::render($view_file);
-		$this->mpdf->WriteHTML($html);
-		// Render the HTML to a PDF
-		$this->mpdf->output($generated_filename, 'D');
-	}
-
-	public function inline($generated_filename, $view_file = NULL)
-	{
-		if (empty($view_file)) $view_file = $this->view_file;
-		// Render the HTML normally
-		$html = parent::render($view_file);
-		$this->mpdf->WriteHTML($html);
-		// Render the HTML to a PDF
-		$this->mpdf->output($generated_filename, 'I');
-		// Necessary to prevent Kohana from overriding the content-type set inside the previous function - we
-		// explictly set it to the correct type here...
-		Request::current()->headers[] = 'Content-type: application/pdf';
-	}
-
+	/*
+	 * Get an instance of MPDF object
+	 */
 	public function get_mpdf()
 	{
 		return $this->mpdf;
 	}
 
-} // End View_MPDF
+	public function __toString()
+	{
+		return $this->as_string();
+	}
+
+	public function as_string($view_file = NULL)
+	{
+		if (empty($view_file)) $view_file = $this->view_file;
+
+		$html = parent::render($view_file);
+		$this->mpdf->WriteHTML($html);
+
+		return $this->mpdf->output(FALSE, 'S');
+	}
+
+	public function download($generated_filename, $view_file = NULL)
+	{
+		if (empty($view_file)) $view_file = $this->view_file;
+
+		$html = parent::render($view_file);
+		$this->mpdf->WriteHTML($html);
+
+		return $this->mpdf->output($generated_filename, 'D');
+	}
+
+	public function inline($generated_filename, $view_file = NULL)
+	{
+		if (empty($view_file)) $view_file = $this->view_file;
+
+		$html = parent::render($view_file);
+		$this->mpdf->WriteHTML($html);
+
+		// Necessary to prevent Kohana from overriding the content-type set inside the previous function - we
+		// explictly set it to the correct type here...
+		Request::$current->headers('content-type', 'application/pdf');
+
+		return $this->mpdf->output($generated_filename, 'I');
+	}
+
+	public function write_to_disk($filename, $view_file = NULL)
+	{
+		if (empty($view_file)) $view_file = $this->view_file;
+
+		$html = parent::render($view_file);
+		$this->mpdf->WriteHTML($html);
+
+		return $this->mpdf->output($filename, 'F');
+	}
+
+} // End View_mPDF_Core
